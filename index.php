e7a0134a7737e7961be746ad73cd8f2be2a9f238
He Gu Yi <eldonreturn@gmail.com>
2013-02-05 16:12:20 +0800

    Two files

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$site = array(
+	'title' => '乒乓球拍底板的特点、比较与选择'
+);
+?>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?php echo $site['title'] ?></title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body class="plain">
+<div class="ezpress_header">
+	<div></div>
+</div>
+</body>
+</html>
